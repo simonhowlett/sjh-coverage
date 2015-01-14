802f138bcbc2127d1d1c9cc6d0209bf87ff32f71
Simon Howlett <simon@showlett.com>
2015-01-13 20:59:01 -0600

    commented out some tests to drop coverage..

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -10,7 +10,7 @@ class IsOddTests(unittest.TestCase):
 
     def testOne(self):
         self.failUnless(IsOdd(1))
-
+'''
     def testtwo(self):
         self.failIf(IsOdd(2))
 
@@ -21,7 +21,7 @@ class IsEvenTests(unittest.TestCase):
 
     def testfour(self):
         self.failIf(IsEven(1))
-
+'''
 def main():
     unittest.main()
 
